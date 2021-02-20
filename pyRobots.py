190334d523f6b4c6524f2cc122b29b7577079c23
Bl1xY <45526353+Bl1xY@users.noreply.github.com>
2021-02-20 04:17:41 +0000

    Update pyRobots.py

--- pyRobots.py
+++ pyRobots.py
@@ -28,25 +28,67 @@ print(banner)
 
 def robotMenu():
 	print(bc.BC + ' 1. ' + bc.GC + 'Quick Scan' + bc.BC + '\t\t\tScan Speed: ' + bc.GC + 'Fast')
+	print('\t' + bc.BC + 'Fast Scans')
+	print('\t' + bc.BC + 'Only Download from robots.txt Entries')
+	print('\t' + bc.BC + 'No Extensions')
+	print('\t' + bc.BC + 'No Request Delay\n')
 	print(bc.BC + ' 2. ' + bc.GC + 'Entended Scan' + bc.BC + '\t\tScan Speed: ' + bc.BC + 'Slow')
+	print('\t' + bc.BC + 'Extended Slower Scans')
+	print('\t' + bc.BC + 'Download from robots.txt Entries + Wordlist Entries')
+	print('\t' + bc.BC + 'Small Extensions Wordlist | Growing Extensions Wordlist')
+	print('\t' + bc.BC + 'No Request Delay\n')
 	print(bc.BC + ' 3. ' + bc.GC + 'Advanced Scan' + bc.BC + '\t\tScan Speed: ' + bc.RC + 'Very Slow\n')
+	print('\t' + bc.BC + 'Slowest Deep Scans')
+	print('\t' + bc.BC + 'Download from robots.txt Entries + Wordlist Entries + All Extensions')
+	print('\t' + bc.BC + 'Set Both Wordlists')
+	print('\t' + bc.BC + 'Set Request Delay\n')
 
 	try:
 		menu = int(input(bc.BC + ' Option: ' + bc.GC))	
 		if menu == 1:
 			os.system('clear')
 			print(banner)
-			quick()
-			
+			try:
+				quick()
+			except KeyboardInterrupt:
+				os.system('clear')
+				print(banner)
+				print(bc.BC + ' Clearing Cache...')
+				os.system('rm -rf __pycache__/')
+				time.sleep(1)
+				print(bc.BC + ' Closing pyRobots...')
+				time.sleep(1)
+				quit()
+
 		elif menu == 2:
 			os.system('clear')
 			print(banner)
-			extensive()
-			
+			try:
+				extensive()
+			except KeyboardInterrupt:
+				os.system('clear')
+				print(banner)
+				print(bc.BC + ' Clearing Cache...')
+				os.system('rm -rf __pycache__/')
+				time.sleep(1)
+				print(bc.BC + ' Closing pyRobots...')
+				time.sleep(1)
+				quit()
+
 		elif menu == 3:
 			os.system('clear')
 			print(banner)
-			advanced()
+			try:
+				advanced()
+			except KeyboardInterrupt:
+				os.system('clear')
+				print(banner)
+				print(bc.BC + ' Clearing Cache...')
+				os.system('rm -rf __pycache__/')
+				time.sleep(1)
+				print(bc.BC + ' Closing pyRobots...')
+				time.sleep(1)
+				quit()
 
 		elif menu == 0:
 			print(bc.BC + ' Closing pyRobots...')
