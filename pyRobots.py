d00b18e31873f1a30d58066e7b21a85e289a0d4f
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-05-28 16:48:48 +0000

    Update pyRobots.py

--- pyRobots.py
+++ pyRobots.py
@@ -2,120 +2,61 @@
 # -*- coding: utf-8 -*-
 
 import sys, os, time
-from quickScan import quick
-from extendedScan import extensive
-from advancedScan import advanced
+from modules.stylesheet import bc, sd, menu
+from modules.quickScan import quick
+banner = sd.banner.logo
+help = menu.helper
+eBan = sd.eBan
+iBan = sd.iBan
+sBan = sd.sBan
 
-class bc:
-	GC = '\033[1;39m'
-	BC = '\033[1;34m'
-	RC = '\033[1;31m'
-
-banner = bc.RC + '''
-               ___       __        __     
-    ___  __ __/ _ \___  / /  ___  / /____ 
-   / _ \/ // / , _/ _ \/ _ \/ _ \/ __(_-< 
-  / .__/\_, /_/|_|\___/_.__/\___/\__/___/ 
- /_/   /___/                              
-'''
-
-infoBanner = bc.BC + " [" + bc.GC + "?" + bc.BC + "]"
-successBanner = bc.BC + " [" + bc.GC + "*" + bc.BC + "]"
-errorBanner = bc.BC + " [" + bc.RC + "!" + bc.BC + "]"
+try:
+	os.mkdir('Pulled-Data')
+except FileExistsError:
+	pass
+except Exception:
+	os.system('clear')
+	print(banner)
+	print(eBan + bc.RC + ' ERROR: ' + bc.BC + 'Failed to create ' + bc.RC + '/Pulled-Data/' + bc.BC + ' Directory\n')
+	quit()
 
 os.system('clear')
 print(banner)
+print(help)
 
 def robotMenu():
-	print(bc.BC + ' 1. ' + bc.GC + 'Quick Scan' + bc.BC + '\t\t\tScan Speed: ' + bc.GC + 'Fast')
-	print('\t' + bc.BC + 'Minimal Scan')
-	print('\t' + bc.BC + 'Only Download from robots.txt Entries')
-	print('\t' + bc.BC + 'No Extensions')
-	print('\t' + bc.BC + 'No Request Delay\n')
-	print(bc.BC + ' 2. ' + bc.GC + 'Entended Scan' + bc.BC + '\t\tScan Speed: ' + bc.BC + 'Slow')
-	print('\t' + bc.BC + 'Extended Scan')
-	print('\t' + bc.BC + 'Download from robots.txt Entries + Wordlist Entries')
-	print('\t' + bc.BC + 'Small Extensions Wordlist | Growing Extensions Wordlist')
-	print('\t' + bc.BC + 'No Request Delay\n')
-	print(bc.BC + ' 3. ' + bc.GC + 'Advanced Scan' + bc.BC + '\t\tScan Speed: ' + bc.RC + 'Very Slow\n')
-	print('\t' + bc.BC + 'Deep Scan')
-	print('\t' + bc.BC + 'Download from robots.txt Entries + Wordlist Entries + All Extensions')
-	print('\t' + bc.BC + 'Set Both Wordlists')
-	print('\t' + bc.BC + 'Set Request Delay\n')
-
 	try:
-		menu = int(input(bc.BC + ' Option: ' + bc.GC))	
-		if menu == 1:
+		host = str(input(bc.BC + " URL: " + bc.GC))
+		if(host == ''):
 			os.system('clear')
 			print(banner)
-			try:
-				quick()
-			except KeyboardInterrupt:
-				os.system('clear')
-				print(banner)
-				print(bc.BC + ' Clearing Cache...')
-				os.system('rm -rf __pycache__/')
-				time.sleep(1)
-				print(bc.BC + ' Closing pyRobots...')
-				time.sleep(1)
-				quit()
-
-		elif menu == 2:
-			os.system('clear')
-			print(banner)
-			try:
-				extensive()
-			except KeyboardInterrupt:
-				os.system('clear')
-				print(banner)
-				print(bc.BC + ' Clearing Cache...')
-				os.system('rm -rf __pycache__/')
-				time.sleep(1)
-				print(bc.BC + ' Closing pyRobots...')
-				time.sleep(1)
-				quit()
-
-		elif menu == 3:
-			os.system('clear')
-			print(banner)
-			try:
-				advanced()
-			except KeyboardInterrupt:
-				os.system('clear')
-				print(banner)
-				print(bc.BC + ' Clearing Cache...')
-				os.system('rm -rf __pycache__/')
-				time.sleep(1)
-				print(bc.BC + ' Closing pyRobots...')
-				time.sleep(1)
-				quit()
-
-		elif menu == 0:
-			print(bc.BC + ' Closing pyRobots...')
-			time.sleep(1)
-			quit()
-	
+			print(eBan + bc.RC + ' ERROR: ' + bc.BC + 'URL value cannot be empty\n')
+			quick()			
+		elif(host.startswith('http://') or host.startswith('https://')):
+			if(host.endswith('/')):
+				host = host
+			else:
+				host = host + '/'
+			quick(host)
 		else:
 			os.system('clear')
 			print(banner)
-			print(errorBanner + bc.RC + "ERROR: " + bc.BC + "Invalid Option" + errorBanner)
-			robotMenu()
-
-	except Exception:
+			print(eBan + bc.RC + ' ERROR: ' + bc.BC + 'Invalid URL value\n')
+			quick()
+	except ValueError:
 		os.system('clear')
 		print(banner)
-		print(errorBanner + bc.RC + "ERROR: " + bc.BC + "Invalid Option" + errorBanner)
+		print(menu)
+		print(eBan + bc.RC + "ERROR: " + bc.BC + 'Value must be INTEGER [' + bc.GC + '0-3' + bc.BC + ']\n')
 		robotMenu()
-
 	except KeyboardInterrupt:
 		os.system('clear')
 		print(banner)
 		print(bc.BC + ' Clearing Cache...')
-		os.system('rm -rf __pycache__/')
-		time.sleep(1)
-		print(bc.BC + ' Closing pyRobots...')
-		time.sleep(1)
+		os.system('rm -rf modules/__pycache__/')
+		time.sleep(0.5)
+		os.system('clear')
+		print(banner)
 		quit()
-
 if __name__ == '__main__':
 	robotMenu()
